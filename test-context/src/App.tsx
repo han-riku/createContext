040c203070af4916a373ac5c1d86d3c81afaca4a
Fan Lu <han_riku@aiful.co.jp>
2025-02-06 11:49:38 +0900

    Routerの作成

--- test-context/src/App.tsx
+++ test-context/src/App.tsx
@@ -1,11 +1,33 @@
 import './App.css'
 
-function App() {
+import Guidance from './Guidance'
+import PageOne from './property/PageOne'
+import PageTwo from './property/PageTwo'
+
+import { RouterProvider, createBrowserRouter, Route, Routes } from 'react-router'
+
+function Root() {
   return (
-    <>
-      
-    </>
+    <Routes>
+      <Route path="uploadForm">
+        <Route path="guidance" element={<Guidance />} />
+        <Route path="pageOne" element={<PageOne />} />
+        <Route path="pageTwo" element={<PageTwo />} />
+      </Route>
+      <Route path="*" element={<div>Not Found</div>} />
+    </Routes>
   )
+}
+
+const router = createBrowserRouter([
+  {
+    path: "*",
+    element: <Root />
+  }
+])
+
+function App() {
+  return <RouterProvider router={router} />
 }
 
 export default App
