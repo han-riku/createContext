35fb63daab80b704e7a8492dc2b22cb13effbb13
Fan Lu <han_riku@aiful.co.jp>
2025-02-06 18:16:18 +0900

    簡単なcss追加

--- test-context/src/Guidance.tsx
+++ test-context/src/Guidance.tsx
@@ -1,13 +1,17 @@
 import { useNavigate } from 'react-router-dom';
 
+import './Guidance.css'
+
 export default function Guidance() {
     const navigate = useNavigate()
 
     return (
         <div>
             <h1>ご案内</h1>
-            <button onClick={() => navigate("/uploadForm/pageOne")}>物件情報登録</button>
-            <button onClick={() => navigate("/uploadForm/counselingPageOne")}>カウンセリング情報登録</button>
+            <div className='guidance'>
+                <button onClick={() => navigate("/uploadForm/pageOne")}>物件情報登録</button>
+                <button onClick={() => navigate("/uploadForm/counselingPageOne")}>カウンセリング情報登録</button>
+            </div>
         </div>
     );
 }
