b6e64b5614d4730c95eb98a668377f111d206971
Fan Lu <han_riku@aiful.co.jp>
2025-02-06 12:44:22 +0900

    onChange作り

--- test-context/src/property/PageOne.tsx
+++ test-context/src/property/PageOne.tsx
@@ -1,14 +1,25 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function PageOne() {
     const navigate = useNavigate()
+
+    const [name, setName] = useState("")
 
     return (
         <>
             <div>
                 <h1>Page One</h1>
                 <label htmlFor="name">Name:</label>
-                <input type="text" id="name" name="name" />
+                <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    onChange={(e) => {
+                        setName(e.target.value)
+                        console.log(name)  
+                    }}
+                />
             </div>
             <div>
                 <button onClick={() => navigate("/uploadForm/pageTwo")}>次へ</button>
--- test-context/src/property/PageTwo.tsx
+++ test-context/src/property/PageTwo.tsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function PageTwo() {
+    const navigate = useNavigate();
+    const [phone, setPhone] = useState("");
+
     return (
         <>
             <div>
                 <h1>Page Two</h1>
                 <label htmlFor="phone">電話番号:</label>
-                <input type="text" id="phone" name="phone" />
+                <input
+                    type="text"
+                    id="phone"
+                    onChange={(e) => {
+                        setPhone(e.target.value);
+                        console.log(phone);
+                    }}
+                />
             </div>
             <div>
-                <button>提出</button>
+                <button onClick={() => navigate("/uploadForm/propertyComplete")}>提出</button>
             </div>
         </>
     );
