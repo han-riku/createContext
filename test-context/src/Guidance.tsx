3c2e216678d03f17808e1e595bb1e635de15a3b4
Fan Lu <han_riku@aiful.co.jp>
2026-05-27 13:18:38 +0900

    Add the Hello World.

--- test-context/src/Guidance.tsx
+++ test-context/src/Guidance.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
 
+import { Button } from '@mui/material';
+
 import './Guidance.css'
 
 export default function Guidance() {
@@ -12,6 +14,7 @@ export default function Guidance() {
             <div className='guidance'>
                 <button onClick={() => navigate("/uploadForm/pageOne")}>物件情報登録</button>
                 <button onClick={() => navigate("/uploadForm/counselingPageOne")}>カウンセリング情報登録</button>
+                <div>Hello World</div>
             </div>
         </div>
     );
