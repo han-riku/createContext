ee5f419c662e84917c46859866713843246a383a
Fan Lu <han_riku@aiful.co.jp>
2026-06-03 11:08:46 +0900

    Add Test Context paragraph to Guidance component

--- test-context/src/Guidance.tsx
+++ test-context/src/Guidance.tsx
@@ -13,6 +13,7 @@ export default function Guidance() {
                 <button onClick={() => navigate("/uploadForm/pageOne")}>物件情報登録</button>
                 <button onClick={() => navigate("/uploadForm/counselingPageOne")}>カウンセリング情報登録</button>
                 <div>Hello World World!</div>
+                <p>Test Context</p>
             </div>
         </div>
     );
